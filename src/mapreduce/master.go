646d78bc45bf003b335e0a1d19196543ee40a131
Caroline-Quigg <carolinequigg@yahoo.com>
2019-04-11 13:20:13 -0700

    fault tolerance added

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -8,6 +8,7 @@ type WorkerInfo struct {
 	// You can add definitions here.
 	failed bool
 	jobId int
+	operation JobType
 }
 
 
@@ -45,7 +46,6 @@ func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
 	//listen for workers & then give them a job! 
 
-
 	mapJob := 0
 	reduceJob := 0
 	for{
@@ -55,43 +55,55 @@ func (mr *MapReduce) RunMaster() *list.List {
 			// register channel address
 			worker := WorkerInfo {address: msg, failed: false, jobId: -1}
 			mr.Workers[msg] = &worker
-			mr.AvailableWorkers =  append(mr.AvailableWorkers, msg)
 			go func(msg string) { mr.jobChannel <- msg }(msg)
 		// worker is ready for a job
 		case msg := <- mr.jobChannel:
 			workerInfo := mr.Workers[msg]
-			if mapJob < mr.nMap {
-				go func (jobId int, worker *WorkerInfo){
+			//delete the worker & roll back to failed job
+			if workerInfo.failed {
+				if (workerInfo.operation == Map){
+					mapJob = min(workerInfo.jobId,mapJob)
+				} else {
+					reduceJob = min(workerInfo.jobId, reduceJob)
+				}
+				fmt.Printf("Removing worker "+ msg + " ...\n")
+				delete(mr.Workers, msg)
+			} else if mapJob < mr.nMap {
+				go func (jobNum int, worker *WorkerInfo){
 					// send RPC to job to do 
 					var reply DoJobReply
 					info := *worker
 					
-					args := DoJobArgs{mr.file, Map, jobId, mr.nReduce }
+					args := DoJobArgs{mr.file, Map, jobNum, mr.nReduce }
 
 					ok := call(info.address, "Worker.DoJob", args, &reply)
 
 					// if RPC fails, log failed job num & index of failed worker
 					if ok == false {
 						fmt.Printf("Map: RPC %s map error\n", info.address)
+						newInfo := WorkerInfo{address: info.address, failed: true, jobId: jobNum,  operation: Map  }
+						*worker = newInfo
 					}
 					go func(msg string) { mr.jobChannel <- msg }(info.address)
 
 				}(mapJob,workerInfo)
-				mapJob+=1
+				mapJob +=1 // idea is don't increment this if the job failed
 				
 			} else if reduceJob < mr.nReduce {
-				go func (jobId int, worker *WorkerInfo){
+				go func (jobNum int, worker *WorkerInfo){
 					// send RPC to job to do 
 					var reply DoJobReply
 					info := *worker
 					
-					args := DoJobArgs{mr.file, Reduce, jobId, mr.nMap }
+					args := DoJobArgs{mr.file, Reduce, jobNum, mr.nMap }
 
 					ok := call(info.address, "Worker.DoJob", args, &reply)
 
 					// if RPC fails, log failed job num & index of failed worker
 					if ok == false {
 						fmt.Printf("Reduce: RPC %s reduce error\n", info.address)
+						newInfo := WorkerInfo{address: info.address, failed: true, jobId: jobNum, operation: Reduce}
+						*worker = newInfo
 					}
 					go func(msg string) { mr.jobChannel <- msg }(info.address)
 
